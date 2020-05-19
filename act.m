86564715a59c587f863f90052a0b5096e5105625
JITHIN K C <jithinkc22@gmail.com>
2020-05-19 23:20:56 +0530

    Add files via upload

---
+++ act.m
@@ -0,0 +1,31 @@
+% key generation using Chaotic map function ()
+%---------------------------------------------------------------------------------------------------------------------------------------
+function out=act(x1,x2,x3,a,b,siz)
+%---------------------------------------------------------------------------------------------------------------------------------------
+% Note: If you are using my code for your system or project, you should always cite my paper as a reference. 
+% K C.Jithin, SyamSankar, "Colour image encryption algorithm combining Arnold map, DNA sequence operation, and a Mandelbrot set", 
+% Journal of Information Security and Applications, Elsevier, Volume 50, February 2020, DOI: https://doi.org/10.1016/j.jisa.2019.102428
+%---------------------------------------------------------------------------------------------------------------------------------------
+% Reading initial & parameter values
+  n=siz*siz;
+% Input the initial values
+for i=1:n
+
+
+% If you need complete code contact me
+
+
+
+
+end
+
+disp(' created by jithin.k.c');
+%---------------------------------------------------------------------------------------------------------------------------------------
+% Output matrix with size 256*256
+
+x1= reshape(x1,[siz,siz]);
+x2= reshape(x2,[siz,siz]);
+x3= reshape(x3,[siz,siz]);
+out=x1;
+end
+%---------------------------------------------------------------------------------------------------------------------------------------
